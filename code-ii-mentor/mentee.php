33038fd8c04760e7e918cda31f6768f9944754b7
earlejam <jearley2@wisc.edu>
2017-11-20 22:42:23 -0600

    Form pages now functional; just spit back a thank you page

--- code-ii-mentor/mentee.php
+++ code-ii-mentor/mentee.php
@@ -11,21 +11,22 @@
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
+
   <nav class="white" role="navigation">
     <div class="nav-wrapper container">
-      <a id="logo-container" href="#" class="brand-logo">CodeForAll</a>
+      <a id="logo-container" href="index.html" class="brand-logo">CodeFor<b><span style="color: #283593;">All</span></b></a>
       <ul class="right hide-on-med-and-down">
         <li><a href="#">About</a></li>
-        <li><a href="mentor.html">Become a Mentor</a></li>
-        <li><a href="mentee.html">Become a Mentee</a></li>
+        <li><a href="mentor.php">Become a Mentor</a></li>
+        <li><a href="mentee.php">Become a Mentee</a></li>
         <li><a href="#">Connect</a></li>
         <li><a href="#">Contact Us</a></li>
       </ul>
 
       <ul id="nav-mobile" class="side-nav">
         <li><a href="#">About</a></li>
-        <li><a href="mentor.html">Become a Mentor</a></li>
-        <li><a href="mentee.html">Become a Mentee</a></li>
+        <li><a href="mentor.php">Become a Mentor</a></li>
+        <li><a href="mentee.php">Become a Mentee</a></li>
         <li><a href="#">Connect</a></li>
         <li><a href="#">Contact Us</a></li>
       </ul>
@@ -33,51 +34,91 @@
     </div>
   </nav>
 
+  <?php
+    $displayForm = True;
+
+    if(isset($_POST['action'])) {
+      if(!empty($_POST['terms'])) {
+
+        $displayForm = False;
+        $first = $_POST['first_name'];
+
+        ?>
+
+        <div class="row">
+          <div class="col s10 m8 offset-m2 offset-s1 center-align">
+            <h2>Thanks, <?php echo $first; ?>!</h2>
+            <h4>We can't wait to connect you with mentors with the expertise you need! Stay tuned.</h4>
+            <img class="responsive-img" src="resources/images/mature-students.jpg">
+          </div>
+        </div>
+        <br>
+        <div class="row">
+          <div class="col s4 m4 offset-m4 offset-s4 center-align">
+            <a class="waves-effect waves-light btn-large" href="index.html">Return to Home</a>
+          </div>
+        </div>
+        <br>
+
+        <?php
+      }
+    }
+
+    if($displayForm) {
+  ?>
+
 <div class="row">
   <div class="col s12 m8 offset-m2">
-    <h1>Become a Mentor</h1>
+    <h1>Become a <b><span style="color: #283593;">Mentee</span></b></h1>
   </div>
 </div>
 <div class="row">
-    <form class="col s12 m8 offset-m2">
+    <form class="col s12 m8 offset-m2" action="mentee.php" method="post">
       <div class="row">
         <div class="input-field col s6">
-          <input id="first_name" type="text" class="validate">
+          <input id="first_name" name="first_name" type="text" class="validate">
           <label for="first_name">First Name</label>
         </div>
         <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate">
+          <input id="last_name" name="last_name" type="text" class="validate">
           <label for="last_name">Last Name</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s6">
-          <input id="email" type="text" class="validate">
+          <input id="email" name="email" type="text" class="validate">
           <label for="email">Email</label>
         </div>
         <div class="input-field col s6">
-          <input id="confirm_email" type="text" class="validate">
+          <input id="confirm_email" name="confirm_email" type="text" class="validate">
           <label for="confirm_email">Confirm Email</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s6">
-          <input id="password" type="password" class="validate">
+          <input id="password" name="password" type="password" class="validate">
           <label for="password">Password</label>
         </div>
         <div class="input-field col s6">
-          <input id="confirm_password" type="password" class="validate">
+          <input id="confirm_password" name="confirm_password" type="password" class="validate">
           <label for="confirm_password">Confirm Password</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="skills" type="text" class="validate">
-          <label for="skills">What subjects would you like to provide mentoring for? (Separate with commas, please)</label>
+          <textarea id="about_self" name="about_self"
+          class="materialize-textarea"></textarea>
+          <label for="textarea1">Please tell us about yourself in a few short sentences.</label>
         </div>
       </div>
       <div class="row">
-        <input type="checkbox" id="terms" />
+        <div class="input-field col s12">
+          <input id="skills" name="skills" type="text" class="validate">
+          <label for="skills">What subjects are you interested in receiving mentoring with? (Separate with commas, please)</label>
+        </div>
+      </div>
+      <div class="row">
+        <input type="checkbox" name="terms" id="terms" />
         <label for="terms">I agree to the CodeForAll <a href="https://www.apple.com/legal/internet-services/itunes/us/terms.html" target="_blank">Terms and Conditions</a>.</label>
       </div>
       <div class="row">
@@ -87,6 +128,10 @@
       </div>
     </form>
   </div>
+
+  <?php
+    }
+   ?>
 
   <footer class="page-footer teal">
     <div class="container">
